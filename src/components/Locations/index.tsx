fe86bd6877c6eb8f9e0e17f103a6abafe4541b57
Harsh Patadia <patadiaharsh.8@gmail.com>
2024-06-04 20:33:33 +0530

    icon added

--- src/components/Locations/index.tsx
+++ src/components/Locations/index.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import SubdirectoryArrowRightIcon from '@mui/icons-material/SubdirectoryArrowRight';
+import './styles.css'
 
 export const Locations = () => {
   return (
     <div className='location-container'>
       <div className='location'>
-        <ul>Manchester</ul>
-        <ul>New York</ul>
-        <ul>Istanbul</ul>
-        <ul>London</ul>
+        <SubdirectoryArrowRightIcon className='icon-sub'/><ul>Manchester</ul>
+        <SubdirectoryArrowRightIcon className='icon-sub'/><ul>New York</ul>
+        <SubdirectoryArrowRightIcon className='icon-sub'/><ul>Istanbul</ul>
+        <SubdirectoryArrowRightIcon className='icon-sub'/><ul>London</ul>
       </div>
     </div>
   )
